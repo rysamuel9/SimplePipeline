d22c1b2c4ac2dff6dbc820b85c021d5887e733e8
Tamba Samuel <47234528+rysamuel9@users.noreply.github.com>
2025-01-13 23:56:14 +0700

    Add project files.

---
+++ SimplePipeline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimplePipeline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
